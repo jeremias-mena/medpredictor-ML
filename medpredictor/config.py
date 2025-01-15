83d8a1330d7ca15446007b805372ce4fb368c3f6
jeremias-mena <menajeremias08@gmail.com>
2025-01-15 11:24:26 -0300

    Modifying file config.py

--- medpredictor/config.py
+++ medpredictor/config.py
@@ -6,5 +6,5 @@ class Config:
     data_dir_cleaned = Path(__file__).resolve().parent / 'data/cleaned'
 
     data_raw_path = data_dir_raw/'diabetes_012_health_indicators_BRFSS2015.csv'
-    data_export_processed = data_dir_processed/'diabetes_processed.csv'
-    data_export_cleaned = data_dir_cleaned/'diabetes_cleaned.csv'
+    data_processed_path = data_dir_processed/'diabetes_processed.csv'
+    data_cleaned_path = data_dir_cleaned/'diabetes_cleaned.csv'
