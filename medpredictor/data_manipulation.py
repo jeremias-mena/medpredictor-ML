96408f70efbf6c6db20ccf4fa56302cbf52581d6
jeremias-mena <menajeremias08@gmail.com>
2025-01-18 20:03:07 -0300

    Adding new method for DataFrameOperation class in data_manipulation.py

--- medpredictor/data_manipulation.py
+++ medpredictor/data_manipulation.py
@@ -1,7 +1,14 @@
+import pandas as pd
+
 class DataFrameOperations:
     def __init__(self, df):
         self.df = df
         pass
+
+    def create_combined_column(self, new_column_name, column_1, column_2,criterion):
+        self.df[new_column_name] = [criterion(value_1, value_2) for value_1, value_2 in zip(self.df[column_1], self.df[column_2])]
+        return
+    
 
     
 
