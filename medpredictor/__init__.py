6fa49f48d8b4bdc569899a86a9f2fdb9ba519013
jeremias-mena <menajeremias08@gmail.com>
2025-01-18 19:29:11 -0300

    Add new modules to __init__.py

--- medpredictor/__init__.py
+++ medpredictor/__init__.py
@@ -1,3 +1,7 @@
 from .config import Config
+from .metrics import Metric, MetricsDisplay
+from .data_manipulation import DataFrameOperations
+from .utils import Utils
+from .graphs import Graph
 
-__all__ = ['Config']
+__all__ = ['Config', 'Metric', 'MetricsDisplay', 'Utils', 'Graph']
