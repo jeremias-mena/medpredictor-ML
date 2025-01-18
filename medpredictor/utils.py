2de7308012b864cdbe5b4ec0958873942eac5ca3
jeremias-mena <menajeremias08@gmail.com>
2025-01-18 18:59:24 -0300

    Adding new Util's methods in utils.py

--- medpredictor/utils.py
+++ medpredictor/utils.py
@@ -22,3 +22,36 @@ class Utils:
             return 'Only eats vegetables'
         else:
             return 'Does not eat fruits neither vegetables'
+
+    """
+    This method labels person's bad habits such as consume alcohol or tobacco having regard his/her survey's answers
+    Parameters
+    value a = answer to the question Do you have X drinks per week? (adult men >= 14 drinks per week and adult women >= 7 drinks per week)
+    value t = answer to the question Have you smoked at least 100 cigarettes in your entire life? 
+    """    
+    def bad_habits_labeling(value_a, value_t):
+        if (value_a == 'Yes') & (value_t == 'Yes'):
+            return 'Consume alcohol and tobacco'
+        elif (value_a == 'Yes') & (value_t == 'No'):
+            return 'Only consume alcohol'
+        elif (value_a == 'No') & (value_t == 'Yes'):
+            return 'Only consume tobacco'
+        else:
+            return 'Does not consume alcohol neither tobacco'
+
+    """
+    This method assigns a label taking into account the age range of the person.
+    Parameters
+    answer = person's age is between x and y.
+    """    
+    def age_range_labeling(answer):
+        if answer in ['18 to 24']:
+            return 'Young'
+        elif answer in ['25 to 29', '30 to 34', '35 to 39']:
+            return 'Young adulthood'
+        elif answer in ['40 to 44', '45 to 49']:
+            return 'Middle adulthood'
+        elif answer in ['50 to 54', '55 to 59']:
+            return 'Late adulthood'
+        else:
+            return 'Old age'
