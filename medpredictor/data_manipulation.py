24cb15d631b569c5ddc6e0d05cfb999e6f1447df
jeremias-mena <menajeremias08@gmail.com>
2025-01-18 23:49:10 -0300

    update(data_manipulation): remove DataFrameOperations's method column_identifier

--- medpredictor/data_manipulation.py
+++ medpredictor/data_manipulation.py
@@ -11,17 +11,7 @@ class DataFrameOperations:
         self.df[new_column_name] = [criterion(value_1, value_2) for value_1, value_2 in zip(self.df[column_1], self.df[column_2])]
         return
    
-    def column_identifier(self, column_1, column_2, values_subset):
-        column_1_values = self.df[column_1].unique()
-        column_2_values = self.df[column_2].unique()
-        
-        if values_subset.issubset(column_1_values):
-            variable_column, filter_column = column_1, column_2
-        elif values_subset.issubset(column_2_values):
-            variable_column, filter_column = column_2, column_1
-        else:
-            raise ValueError("Cannot identify columns properly.")
-        return variable_column, filter_column
+  
 
     
 
