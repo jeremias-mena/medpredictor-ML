af9bcbdd7eb97ec5f760d2660da90e769854d1e9
jeremias-mena <menajeremias08@gmail.com>
2025-01-19 20:54:57 -0300

    feat(metrics): add new methods show_metric and show_metrics to MetricsDisplay

--- medpredictor/metrics.py
+++ medpredictor/metrics.py
@@ -1,12 +1,17 @@
 from IPython.display import Markdown
 
-class Metric:
-    
-    def __init__(self):
-        pass
-
 class MetricsDisplay:
     
     def __init__(self):
         pass
+    
+    @staticmethod
+    def show_metric(name_metric, metric):
+        Markdown(f"{name_metric}: {metric}%")
+        return
 
+    @staticmethod
+    def show_metrics(metrics):
+        for name, value in metrics.items():
+            Markdown(f"{name}: {value}%")
+        return
