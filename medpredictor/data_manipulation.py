3638bb912cadb67858e601907d7dfc22bcceeb60
jeremias-mena <menajeremias08@gmail.com>
2025-01-18 23:42:36 -0300

    feat(data_manipulation): add new DataFrameOperations's method called column_identifier

--- medpredictor/data_manipulation.py
+++ medpredictor/data_manipulation.py
@@ -10,7 +10,7 @@ class DataFrameOperations:
     def create_combined_column(self, new_column_name, column_1, column_2,criterion):
         self.df[new_column_name] = [criterion(value_1, value_2) for value_1, value_2 in zip(self.df[column_1], self.df[column_2])]
         return
-    
+   
     def column_identifier(self, column_1, column_2, values_subset):
         column_1_values = self.df[column_1].unique()
         column_2_values = self.df[column_2].unique()
