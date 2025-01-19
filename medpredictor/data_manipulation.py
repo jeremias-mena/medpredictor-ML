1491fb3a874a19c42918657656d16294e763b211
jeremias-mena <menajeremias08@gmail.com>
2025-01-19 15:26:54 -0300

    update(data_manipulation): change return's method create_combined_column

--- medpredictor/data_manipulation.py
+++ medpredictor/data_manipulation.py
@@ -9,7 +9,7 @@ class DataFrameOperations:
 
     def create_combined_column(self, new_column_name, column_1, column_2, criterion):
         self.df[new_column_name] = [criterion(value_1, value_2) for value_1, value_2 in zip(self.df[column_1], self.df[column_2])]
-        return 
+        return self.df
     
     def column_identifier(self, column_1, column_2, values_subset):
         column_1_values = self.df[column_1].unique()
