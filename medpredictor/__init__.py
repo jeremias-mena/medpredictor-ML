1c6cc5a22ef8920ba0b86b1d87227f2acc4f68de
jeremias-mena <menajeremias08@gmail.com>
2025-01-25 12:08:45 -0300

    update(files): remove metrics.py and change __init__.py config

--- medpredictor/__init__.py
+++ medpredictor/__init__.py
@@ -1,11 +1,9 @@
 from .config import Config
-from .metrics import MetricsDisplay
 from .data_manipulation import DataFrameOperations
 from .utils import Utils
 from .graphs import Graph
 
 __all__ = ['Config', 
-           'MetricsDisplay', 
            'Utils', 
            'Graph', 
            'DataFrameOperation'
