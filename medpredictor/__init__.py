0d8f380520d37eb03e5be10ff9517ce5fd852182
jeremias-mena <menajeremias08@gmail.com>
2025-02-06 16:34:35 -0300

    update(init): add new module encoders.py

--- medpredictor/__init__.py
+++ medpredictor/__init__.py
@@ -2,13 +2,11 @@ from .config import Config
 from .data_manipulation import DataFrameOperations
 from .utils import Utils
 from .graphs import Graph
-from .models import ModelOperations
 from .encoders import Encoder
 
 __all__ = ['Config', 
            'Utils', 
            'Graph', 
            'DataFrameOperations',
-           'ModelOperations',
            'Encoder'
            ] 
