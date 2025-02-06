7fe0d755240876fe745e0252f8f3250e0e121323
jeremias-mena <menajeremias08@gmail.com>
2025-02-06 15:50:24 -0300

    update(init): add new module ModelOperations

--- medpredictor/__init__.py
+++ medpredictor/__init__.py
@@ -2,9 +2,11 @@ from .config import Config
 from .data_manipulation import DataFrameOperations
 from .utils import Utils
 from .graphs import Graph
+from .models import ModelOperations
 
 __all__ = ['Config', 
            'Utils', 
            'Graph', 
-           'DataFrameOperations'
+           'DataFrameOperations',
+           'ModelOperations'
            ] 
