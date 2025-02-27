c5a6a03a7bd82c364d7dec9a7e977e9d43e0a86f
jeremias-mena <menajeremias08@gmail.com>
2025-02-27 15:11:38 -0300

    update(views): add HomeView, UserFormView and QuestionFormView

--- medpredictor/medpredictor-app/medpredapp/views.py
+++ medpredictor/medpredictor-app/medpredapp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def HomeView(request):
+    return render(request, "home.html")
+
+def UserFormView (request):
+    return render(request, "user_form.html")
+
+def QuestionFormView(request):
+    return render(request, "question_form.html")
