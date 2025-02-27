648b4f34a33b5d9df1f0d08c70a7509de33bb110
jeremias-mena <menajeremias08@gmail.com>
2025-02-27 12:27:00 -0300

    update(models): add new foreign key yo UserDB class

--- medpredictor/medpredictor-app/medpredapp/models.py
+++ medpredictor/medpredictor-app/medpredapp/models.py
@@ -1,15 +1,5 @@
 from django.db import models
 
-class UserDB(models.Model):
-    sex = {'Male': 'Male',
-           'Female': 'Female'}
-    first_name = models.CharField(verbose_name="First Name", max_length=75, null=False, blank=False)
-    last_name = models.CharField(verbose_name="Last Name", max_length=75, null=False, blank=False)
-    sex = models.CharField(verbose_name="Sex", max_length=10, choices=sex, null=False, blank=False)
-    age = models.CharField(verbose_name="Age", max_length=10, null=False, blank=False)
-    birth_date = models.DateField(verbose_name="Birth date", null=False, blank=False)
-    email = models.EmailField(verbose_name="Email", null=False, blank=False)
-
 class AnswersDB(models.Model):
     answers_yn = {'Yes': 'Yes',
                   'No': 'No'}
@@ -34,3 +24,14 @@ class AnswersDB(models.Model):
     diffwalk = models.CharField(verbose_name="Difficulty walking", max_length=3, choices=answers_yn, null=False, blank=False)
     physact = models.CharField(verbose_name="Physical activity", max_length=3, choices=answers_yn, null=False, blank=False)
     healtstatus = models.CharField(verbose_name="General health status", max_length=10, choices=answers_status, null=False, blank=False)
+
+class UserDB(models.Model):
+    sex = {'Male': 'Male',
+           'Female': 'Female'}
+    first_name = models.CharField(verbose_name="First Name", max_length=75, null=False, blank=False)
+    last_name = models.CharField(verbose_name="Last Name", max_length=75, null=False, blank=False)
+    sex = models.CharField(verbose_name="Sex", max_length=10, choices=sex, null=False, blank=False)
+    age = models.CharField(verbose_name="Age", max_length=10, null=False, blank=False)
+    birth_date = models.DateField(verbose_name="Birth date", null=False, blank=False)
+    email = models.EmailField(verbose_name="Email", null=False, blank=False)
+    form_answers = models.ForeignKey(AnswersDB, on_delete=models.CASCADE)
