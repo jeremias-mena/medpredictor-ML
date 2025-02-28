f7321f89c547272623ed5e85275cbab1464642ec
jeremias-mena <menajeremias08@gmail.com>
2025-02-28 11:26:41 -0300

    update(urls): change paths from urlpatterns

--- medpredictor/medpredictor-app/backend/urls.py
+++ medpredictor/medpredictor-app/backend/urls.py
@@ -4,7 +4,7 @@ from medpredapp.views import HomeView, UserFormView, QuestionFormView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', HomeView),
-    path('registeruser', UserFormView),
-    path('registeranswers', QuestionFormView)
+    path('', HomeView.as_view(), name='home'),
+    path('registeruser', UserFormView.as_view(), name='register_user'),
+    path('registeranswers', QuestionFormView.as_view(), name='register_answers')
 ]
