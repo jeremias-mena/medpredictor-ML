0efdc4743932fd32748f0da2a25eef634206108c
jeremias-mena <menajeremias08@gmail.com>
2025-03-30 19:47:32 -0300

    update(predict): add new functions to make predictions

--- medpredictor/src/predict.py
+++ medpredictor/src/predict.py
@@ -1,27 +1,36 @@
 import pickle
 import pandas as pd
 
-path_h1_model = './src/models/knn_h1_model.pkl'
-path_h2_model = './src/models/rf_h2_model.pkl'
-path_h3_model = './src/models/rf_h3_model.pkl'
 
-path_survey_answers = './src/answers/answers_codified.csv'
+def make_h1_prediction():
+    path_survey_answers = './src/answers/answers_codified.csv'
+    path_h1_model = './src/models/knn_h1_model.pkl'
+    
+    with open(path_h1_model, "rb") as model_h1_file:
+        model_h1 = pickle.load(model_h1_file)
+    
+    y = pd.read_csv(path_survey_answers)
+    diabetes = model_h1.predict(y)
+    return diabetes
 
-with open(path_h1_model, "rb") as model_h1_file:
-    model_h1 = pickle.load(model_h1_file)
+def make_h2_prediction():
+    path_survey_answers = './src/answers/answers_codified.csv'
+    path_h2_model = './src/models/rf_h2_model.pkl'
 
-with open(path_h2_model, "rb") as model_h2_file:
-    model_h2 = pickle.load(model_h2_file)
+    with open(path_h2_model, "rb") as model_h2_file:
+        model_h2 = pickle.load(model_h2_file)
 
-with open(path_h3_model, "rb") as model_h3_file:
-    model_h3 = pickle.load(model_h3_file)
+    y = pd.read_csv(path_survey_answers)
+    stroke = model_h2.predict(y)
+    return stroke
 
-y = pd.read_csv(path_survey_answers)
+def make_h3_prediction():
+    path_survey_answers = './src/answers/answers_codified.csv'
+    path_h3_model = './src/models/rf_h3_model.pkl'
 
-diabetes = model_h1.predict(y)
-stroke = model_h2.predict(y)
-heart_attack = model_h3.predict(y)
-
-
-
-
+    with open(path_h3_model, "rb") as model_h3_file:
+        model_h3 = pickle.load(model_h3_file)
+    
+    y = pd.read_csv(path_survey_answers)
+    heart_attack = model_h3.predict(y)
+    return heart_attack
